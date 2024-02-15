3eb01d2c0e39f46c809964e33a0df1de483f8f8f
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-15 02:12:35 +0200

    added game cpt (need to flex those cards though)

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,13 +1,22 @@
 import { Component, Input } from "@angular/core";
-
+import { CardComponent } from "../card/card.component";
+import { cards } from "../../cards.service";
+import { NgFor } from "@angular/common";
 @Component({
   selector: 'game',
   standalone: true,
   templateUrl: './game.component.html',
-  styles: []
+  imports: [CardComponent, NgFor],
+  styles: [`.game-card  
+              width: 30%
+              height: 100px
+              border: 1px solid black
+              border-radius: 12px`]
 })
 
 export class GameComponent {
   @Input() numOfCards! : number;
-
+  cards = cards;
+  counter = (num:number) => new Array(num)
+  
 }
