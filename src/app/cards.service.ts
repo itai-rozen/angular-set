7d5c4269b0fbb8fbe62390792e39d3248ab8b01f
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-15 02:10:56 +0200

    added some cards (maybe create a function to fill all optional cards?)

---
+++ src/app/cards.service.ts
@@ -0,0 +1,21 @@
+import { Card } from "./types/types"
+export const cards: Card[] = [
+  {
+    shape: 'diamond',
+    fill: 'full',
+    numOfShapes: 1,
+    color:'blue'
+  },
+  {
+    shape: 'diamond',
+    fill: 'full',
+    numOfShapes: 2,
+    color:'blue'
+  },
+  {
+    shape: 'diamond',
+    fill: 'full',
+    numOfShapes: 3,
+    color:'blue'
+  }
+]
