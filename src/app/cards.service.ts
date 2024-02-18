86cdd19436d81ff5fea4e6a45bcaca821ac57d09
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-18 17:43:53 +0200

    made a function that gets all the possible cards

--- src/app/cards.service.ts
+++ src/app/cards.service.ts
@@ -1,21 +1,35 @@
 import { Card } from "./types/types"
-export const cards: Card[] = [
-  {
-    shape: 'diamond',
-    fill: 'full',
-    numOfShapes: 1,
-    color:'blue'
-  },
-  {
-    shape: 'diamond',
-    fill: 'full',
-    numOfShapes: 2,
-    color:'blue'
-  },
-  {
-    shape: 'diamond',
-    fill: 'full',
-    numOfShapes: 3,
-    color:'blue'
+const configs = {
+  shape: ['diamond', 'oval', 'squiggle'],
+  number: [1,2,3],
+  fill: ['solid', 'striped', 'open'],
+  color: ['red', 'green', 'blue']
+}
+
+const getCards = () => {
+  const cards = [];
+  for (let i = 0; i < configs.shape.length; i++) {
+    for (let j = 0; j < configs.number.length; j++) {
+      for (let k = 0; k < configs.fill.length; k++) {
+        for (let l = 0; l < configs.color.length; l++){
+          const card: Card = {
+            shape: configs.shape[i] as  Card['shape'],
+            numOfShapes: configs.number[j] as Card['numOfShapes'],
+            fill: configs.fill[k] as Card['fill'],
+            color: configs.color[l] as Card['color']
+          }
+          cards.push(card)
+        }
+      }
+    }
   }
-]
+  return cards;
+} 
+
+// export const cards: Card[] = getCards().sort(() => 0.5 - Math.random());
+export const cards: Card[] = [{
+  shape:'squiggle' as Card['shape'], 
+  numOfShapes: 3 as Card['numOfShapes'], 
+  fill:'striped' as Card['fill'], 
+  color: 'red' as Card['color']
+}];
