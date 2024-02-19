7e038f1d03870b62b6b31238bc46b20bf1d4eadc
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-19 16:46:06 +0200

    added multiplayer (to implement online 2 players game in the future!)

---
+++ src/app/components/multiplayer/multiplayer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multiplayer',
+  standalone: true,
+  imports: [],
+  templateUrl: './multiplayer.component.html',
+  styleUrl: './multiplayer.component.sass'
+})
+export class MultiplayerComponent {
+
+}
