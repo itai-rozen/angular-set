1bdd1d474b26a11b04eb4fa2ccc51ee0f709e577
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-19 16:44:14 +0200

    added paths & components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
+import { InstructionsComponent } from './components/instructions/instructions.component';
+import { MultiplayerComponent } from './components/multiplayer/multiplayer.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'game', component: GameComponent},
+  {path: 'instructions', component: InstructionsComponent},
+  {path: 'multiplayer', component: MultiplayerComponent},
+];
