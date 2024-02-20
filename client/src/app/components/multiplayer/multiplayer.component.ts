a21f21bef9bee81c0913e3a8e0615b3a03a1e7f9
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-20 17:54:15 +0200

    if no game created, user has a button that creates a random uuid and redirects to it

--- client/src/app/components/multiplayer/multiplayer.component.ts
+++ client/src/app/components/multiplayer/multiplayer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { SocketioService } from '../../services/socketio.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-multiplayer',
   standalone: true,
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './multiplayer.component.sass'
 })
 export class MultiplayerComponent {
+  constructor(
+    private socketIoService: SocketioService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){}
+  gameId: string = this.route.snapshot.paramMap.get('gameId') ?? '';
+  ngOnInit() {
 
+    // this.socketIoService.connect()
+  }
+
+  onCreateGame = () => {
+    this.gameId = crypto.randomUUID()
+    this.router.navigate(['/multiplayer', this.gameId])
+  }
 }
