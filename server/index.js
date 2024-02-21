92f64720d1120ec6f0c607947e82e789eed6e3a7
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-21 19:38:00 +0200

    added some socket events for room creation, join, leave

--- server/index.js
+++ server/index.js
@@ -4,15 +4,44 @@ const io = require('socket.io')(httpServer, {
   cors: true,
   origins: ['*']
 })
+const { createGame } = require('./cards')
 
-
+const rooms = {}
 io.on('connection', (socket) => {
   console.log('user connected!')
 
   socket.on('joinGame', ((gameId) => {
+    rooms[gameId.gameId] = (rooms[gameId.gameId]) ? rooms[gameId.gameId] + 1 : 1
     socket.join(gameId)
     socket.to(gameId).emit('joinGame', 'a player joined the game!')
+    console.log('active players: ', rooms)
   }))
+
+  socket.on('leaveGame', ((gameId) => {
+    rooms[gameId.gameId] = (rooms[gameId.gameId]) ? rooms[gameId.gameId] - 1 : 0
+    if (!rooms[gameId.gameId])
+      delete rooms[gameId.gameId]
+    socket.to(gameId).emit('leaveGame', 'a player left the game!')
+    console.log('active players: ', rooms)
+  }))
+
+  socket.on('startGame', (async ({gameId}) => {
+    const imgNums = await createGame();
+    io.to(gameId).emit('startGame', imgNums)
+  }))
+
+  socket.on('getRooms', () => {
+    console.log('getting rooms! ', rooms)
+    io.emit('getRooms', Object.entries(rooms))
+  })
+
+  socket.on('createRoom', (gameId) => {
+    rooms[gameId] = 0
+    console.log('new room! \n', gameId)
+    io.emit('getRooms', Object.entries(rooms)) 
+  })
+
+
 })
 const PORT = 3000
 httpServer.listen(PORT, () => console.log('server listens on port: ', PORT))
