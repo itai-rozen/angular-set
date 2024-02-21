ac3ef9d25bed0cffd88e4daac062d89fb0ddeb9c
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-21 19:36:51 +0200

    added joining\starting\leacing funcs (needs work)

--- client/src/app/services/socketio.service.ts
+++ client/src/app/services/socketio.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Socket, io } from 'socket.io-client';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,5 +9,30 @@ export class SocketioService {
 
   connect(){
     this.socket = io('http://localhost:3000')
+  }
+
+  joinGame(gameId: string) {
+    this.socket.emit('joinGame', { gameId })
+  }
+
+  startGame(gameId: string) {
+    this.socket.emit('startGame', { gameId });
+  }
+
+  leaveGame(gameId: string) {
+    this.socket.emit('leaveGame', { gameId })
+  }
+
+  createRoom(gameId: string) {
+    console.log('inside socketservice create room :', gameId)
+    this.socket.emit('createRoom', gameId)
+  }
+
+  receiveRooms() {
+    return new Observable((observer) => {
+      this.socket.on('getRooms', (rooms) => {
+        observer.next(rooms)
+      })
+    })
   }
 }
