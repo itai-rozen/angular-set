1d12daa4c31c6f03d4ea59a4b0afaaf19cd1ccce
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-21 19:36:23 +0200

    added functions for room creatinge\joining\leavng

--- client/src/app/components/multiplayer/multiplayer.component.ts
+++ client/src/app/components/multiplayer/multiplayer.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { SocketioService } from '../../services/socketio.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { getCardsFromImgNumbers } from '../../cards.service';
+import { Card, Room } from '../../types/types';
+import { GameComponent } from '../game/game.component';
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-multiplayer',
+  selector: 'multiplayer',
   standalone: true,
-  imports: [],
+  imports: [GameComponent, CommonModule],
   templateUrl: './multiplayer.component.html',
   styleUrl: './multiplayer.component.sass'
 })
@@ -14,14 +18,40 @@ export class MultiplayerComponent {
     private route: ActivatedRoute,
     private router: Router
   ){}
-  gameId: string = this.route.snapshot.paramMap.get('gameId') ?? '';
-  ngOnInit() {
+  gameId: string = this.route.snapshot.paramMap.get('gameId') ?? ''
+  cards: Card[] = []
+  rooms: Room[] = []
 
-    // this.socketIoService.connect()
+  ngOnInit() {
+    this.socketIoService.connect()
+
+    if (this.gameId)
+      this.socketIoService.joinGame(this.gameId)
+    else 
+      this.receiveRooms()
   }
 
-  onCreateGame = () => {
+  ngOnDestroy() {
+    console.log('left!!')
+    this.socketIoService.leaveGame(this.gameId)
+  }
+
+  onJoinGame = (gameId: string)  => {
     this.gameId = crypto.randomUUID()
-    this.router.navigate(['/multiplayer', this.gameId])
+    this.router.navigate(['/multiplayer', gameId])
+  }
+
+  receiveRooms = () => {
+    this.socketIoService.receiveRooms().subscribe((rooms) => {
+      console.log('rooms from server: ', rooms)
+      this.rooms = rooms as Room[];
+    })
+  }
+
+  onCreateRoom = () => {
+    console.log('clicked create room')
+    const newRoom = crypto.randomUUID()
+    this.socketIoService.createRoom(newRoom);
+    this.receiveRooms()
   }
 }
