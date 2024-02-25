a0f67b8e76777de737f7f7109dfd2d86cc6dbbe6
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-25 19:40:53 +0200

    added alot of console logs for debugging

--- server/index.js
+++ server/index.js
@@ -9,12 +9,18 @@ const { createGame } = require('./cards')
 const rooms = {}
 
 io.on('connection', (socket) => {
-  console.log('user connected!')
+  console.log('user connected!!! \nrooms: ', rooms)
 
-  socket.on('joinGame', ((gameId, playerId) => {
-    const gameObj = rooms[gameId.gameId] || null
+  socket.on('joinGame', (({gameId, playerId}) => {
+    console.log('rooms @joinGame', rooms)
+    console.log('game ID @joinGame(): ', gameId)
+    console.log('player ID @joinGame(): ', playerId)
+    const gameObj = rooms[gameId] || null
     const playerObj = gameObj?.activePlayers[playerId] || null
-
+    
+    console.log('game obj @joinGame(): ', gameObj)
+  
+    console.log('playerObj obj @joinGame(): ', playerObj)
     if (gameObj && !playerObj) {
         gameObj.activePlayers[playerId] = {
       _id: playerId,
@@ -28,19 +34,28 @@ io.on('connection', (socket) => {
     // console.log('active players: ', rooms)
   }))
 
-  socket.on('leaveGame', ((gameId, playerId) => {
-    const gameObj = rooms[gameId.gameId] || null
+  socket.on('leaveGame', (({gameId, playerId}) => {
+    const gameObj = rooms[gameId] || null
     const playerObj = gameObj?.activePlayers[playerId] || null
+    console.log('game id @leaveGame: ', gameId)
     if (gameObj && playerObj)
       delete playerObj
-    if ( Object.keys(rooms[gameId.gameId]?.activePlayers).length === 0 ) // activeplayers prop is empty object = all players left the game
+    if (playerObj && Object.keys(rooms[gameId]?.activePlayers)?.length === 0 ) // activeplayers prop is empty object = all players left the game
       delete gameObj
     socket.to(gameId).emit('leaveGame', 'a player left the game!')
     // console.log('active players: ', rooms)
   }))
 
   socket.on('startGame', (({gameId, cards}) => {
-    rooms[gameId].cards = cards
+    console.log('game id @startGame: ', gameId)
+    console.log('cards @startGame: ', cards[0])
+    console.log('room: ', rooms)
+    if (rooms[gameId]) {
+      console.log('found the room! setting the cards')
+      rooms[gameId].cards = cards 
+    } else {
+      console.log('no such game room!')
+    }
     io.emit('startGame', rooms)
   }))
 
@@ -50,15 +65,15 @@ io.on('connection', (socket) => {
   })
 
   socket.on('createRoom', (gameId) => {
+    console.log('Create Room! *********\ngame id: ', gameId)
     rooms[gameId] = {
       activePlayers: {},
       cards: []
     }
     console.log('new room! \n', gameId)
+    console.log('rooms @pmulcreateRoom: ', rooms)
     io.emit('getRooms', rooms) 
   })
-
-
 })
 const PORT = 3000
 httpServer.listen(PORT, () => console.log('server listens on port: ', PORT))
