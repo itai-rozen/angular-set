05c5d8f062bdd678b142a508af6a064ba8af13bf
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-26 16:01:51 +0200

    turned multiplayer path to lobby & gameroom paths

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
 import { InstructionsComponent } from './components/instructions/instructions.component';
-import { MultiplayerComponent } from './components/multiplayer/multiplayer.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { LobbyComponent }  from './components/lobby/lobby.component';
+import { GamesRoomListComponent } from './components/games-room-list/games-room-list.component';
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'game', component: GameComponent},
   {path: 'instructions', component: InstructionsComponent},
-  {path: 'multiplayer', component: MultiplayerComponent},
-  {path: 'multiplayer/:gameId', component: MultiplayerComponent},
+  {path: 'rooms/:gameId', component: GamesRoomListComponent},
+  {path: 'lobby', component: LobbyComponent},
 ];
