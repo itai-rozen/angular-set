ffcc0250b5a84840c0217fced6ecb72a87c3ba7e
itai rozen <itai.rozen@mail.huji.ac.il>
2024-03-12 18:16:46 +0200

    start game btn only active if there is more than 1p inside gameroom

--- client/src/app/components/games-room-list/games-room-list.component.ts
+++ client/src/app/components/games-room-list/games-room-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { GameObjType, PlayersObjType, RoomsObjType } from '../../types/types';
 import { SocketioService } from '../../services/socketio.service';
 import { GameComponent } from '../game/game.component';
 import { cards, shuffle } from '../../cards.service';
+import { getNumOfActivePlayers } from '../../services/utils.service';
 @Component({
   selector: 'games-room-list',
   standalone: true,
@@ -20,33 +20,37 @@ export class GamesRoomListComponent implements OnInit{
     this.router = inject(Router)
   }
 
-  gameId: string = this.route.snapshot.paramMap.get('gameId') ?? ''
-  game: any = {} 
+  gameId: string = this.route.snapshot.paramMap.get('gameId') ?? '';
+  game: any = {};
+  getNumOfActivePlayers: Function = getNumOfActivePlayers;
 
   ngOnInit(): void {
     this.socketService.connect()
-    this.socketService.joinGame(this.gameId, localStorage['active-player'])
-    this.receiveUpdateGame(true)  
-    console.log('game room init!!=')
+    if (this.gameId) {
+      this.socketService.joinGame(this.gameId, localStorage['active-player'])
+      this.receiveUpdateGame(true)  
+      console.log('game room init!!')
+    }
   }
 
   onStartGame = () => {
     console.log('cards @onstartgame: ', cards)
     this.socketService.startGame(this.gameId, shuffle(cards))
     this.receiveUpdateGame()
-    
   } 
 
   receiveUpdateGame(first: boolean = false) {
     // this.game = this.socketService.receiveGame(this.gameId)
     // console.log('updated game!\n: ', this.game)
     this.socketService.receiveGame(this.gameId, first).subscribe((updateGame) => {
+      console.log('receitve game!')
       console.log('updated game: ', updateGame)
       this.game = updateGame as any
     })
   }
 
   onLeaveGame() {
+    this.socketService.leaveGame(this.gameId, localStorage['active-player']);
     this.router.navigate(['/lobby'])
   }
 }
