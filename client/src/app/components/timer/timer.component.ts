fd9663dcd8c83406141df3de72b381b82415d47f
itai rozen <itai.rozen@mail.huji.ac.il>
2024-03-19 22:52:19 +0200

    moved styles to separate file

--- client/src/app/components/timer/timer.component.ts
+++ client/src/app/components/timer/timer.component.ts
@@ -4,44 +4,31 @@ import { Component, Input, Output, OnChanges, SimpleChanges, EventEmitter} from 
   standalone: true,
   selector: 'timer',
   templateUrl: './timer.component.html',
-  styles: [`
-  .timer-container
-    display: block
-    width: 100%
-    display: flex
-    justify-content: center
-    align-items: center
-  `, `
-  .timer 
-    border: 2px solid black
-    border-radius: 10px
-    padding: 1em
-    margin: 1em
-  `]
+  styleUrl: './timer.component.sass'
 })
 
 export class TimerComponent implements OnChanges {
-  @Input() secsRemaining: number = 0
-  @Output() secsRemainingEvent = new EventEmitter<number>
+  @Input() secsRemaining: number = 0;
+  @Output() secsRemainingEvent = new EventEmitter<number>;
 
   intervalId: null|ReturnType<typeof setInterval> = null;
   startTimer() {
     this.intervalId =  setTimeout(() => {
-      this.secsRemainingEvent.emit(this.secsRemaining-1)
+      this.secsRemainingEvent.emit(this.secsRemaining-1);
     },1000)
   }
   stopTimer = () => {
     if (this.intervalId)
-      clearInterval(this.intervalId!)
+      clearInterval(this.intervalId!);
 
   } 
 
   ngOnChanges(changes: SimpleChanges) {
-    const { secsRemaining } = changes
+    const { secsRemaining } = changes;
     if (secsRemaining.currentValue > 0) {
-      this.startTimer()
+      this.startTimer();
     }
     else
-      this.stopTimer()
+      this.stopTimer();
   }
 }
